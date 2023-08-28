c32b76eecef1623eb3ca3f6214ecbf00b6a10253
erik.juvan <erik.juvan@gorenje.com>
2023-08-29 00:39:38 +0200

    Split basics, add openbox, add back chromium

--- minimal_arch_install.sh
+++ minimal_arch_install.sh
@@ -8,6 +8,7 @@ read -n 1 -r -p "Wipe drive and perform clean base install [y/N]? "
 echo # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+
 	# Print commands as they are executed
 	set -x
 
@@ -33,51 +34,72 @@ then
 	# Add new user...
 	read -p "Add user: " user
 	echo "Enter password for $user: "
-	arch-chroot /mnt /bin/bash -c "useradd -m -s /bin/fish $user"
+	arch-chroot /mnt /bin/bash -c "useradd -m -s /bin/bash $user"
 	arch-chroot /mnt /bin/bash -c "passwd $user"
-	arch-chroot /mnt /bin/bash -c "sed -i 's/root ALL=(ALL:ALL) ALL/root ALL=(ALL:ALL) ALL\n$user ALL=(ALL:ALL) ALL/' /etc/sudoers"
 	# Base install finished...
 	echo "Minimal install finished."
 	echo
-fi
 
-# Install grub?
-read -n 1 -r -p "Install grub [y/N]? "
-echo # move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	pacstrap /mnt grub
- 	# Setup grub
-	arch-chroot /mnt /bin/bash -c "grub-install --target=i386-pc /dev/sda"
-	arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
-fi
+	# Install grub?
+	read -n 1 -r -p "Install grub [y/N]? "
+	echo # move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		pacstrap /mnt grub
+		# Setup grub
+		arch-chroot /mnt /bin/bash -c "grub-install --target=i386-pc /dev/sda"
+		arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
+	fi
 
-# Install dhcpcd?
-read -n 1 -r -p "Install dhcpcd [y/N]? "
-echo # move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	pacstrap /mnt dhcpcd
- 	# Enable dhcpcd
-	arch-chroot /mnt /bin/bash -c "systemctl enable dhcpcd"	
-fi
+	# Install dhcpcd?
+	read -n 1 -r -p "Install dhcpcd [y/N]? "
+	echo # move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		pacstrap /mnt dhcpcd
+		# Enable dhcpcd
+		arch-chroot /mnt /bin/bash -c "systemctl enable dhcpcd"	
+	fi
 
-# Install true basics?
-read -n 1 -r -p "sudo fish neovim [y/N]? "
-echo # move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	pacstrap /mnt sudo fish neovim
-fi
+	# Install sudo?
+	read -n 1 -r -p "Install sudo [y/N]? "
+	echo # move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		pacstrap /mnt sudo
+		# Add user to sudoers
+		arch-chroot /mnt /bin/bash -c "sed -i 's/root ALL=(ALL:ALL) ALL/root ALL=(ALL:ALL) ALL\n$user ALL=(ALL:ALL) ALL/' /etc/sudoers"
+	fi
 
-# Disable printing of commands, since these next ones are too verbose
-set +x
+	# Install fish?
+	read -n 1 -r -p "Install fish [y/N]? "
+	echo # move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		pacstrap /mnt fish
+		# Change user shell to fish
+		arch-chroot /mnt /bin/bash -c "chsh -s /bin/fish $user"
+	fi
+
+	# Install neovim?
+	read -n 1 -r -p "Install neovim [y/N]? "
+	echo # move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		pacstrap /mnt neovim
+	fi
+
+	# Disable printing of commands, since these next ones are too verbose
+	set +x
+
+fi
 
 # Install additional packages?
 read -n 1 -r -p "Install additional packages [y/N]? "
 echo # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+
 	# Mount, in case we skipped the base install
 	mount /dev/sda1 /mnt 2> /dev/null
 
@@ -93,9 +115,11 @@ then
 	echo # move to a new line
 	read -n 1 -r -p "xfce4 [y/N]? " xfce4
 	echo # move to a new line
-	read -n 1 -r -p "chromium [y/N]? " chromium
+	read -n 1 -r -p "openbox [y/N]? " openbox
 	echo # move to a new line
-    read -n 1 -r -p "lightdm [y/N]? " lightdm
+	read -n 1 -r -p "lightdm [y/N]? " lightdm
+	echo # move to a new line
+	read -n 1 -r -p "chromium [y/N]? " chromium
 	echo # move to a new line
 	
 	if [[ $exa =~ ^[Yy]$ ]]
@@ -122,10 +146,19 @@ then
 	then
 		pacstrap /mnt --needed xfce4
 	fi
+	if [[ $openbox =~ ^[Yy]$ ]]
+	then
+		pacstrap /mnt --needed openbox
+	fi
 	if [[ $lightdm =~ ^[Yy]$ ]]
 	then
 		pacstrap /mnt --needed lightdm-gtk-greeter
 	fi
+	if [[ $chromium =~ ^[Yy]$ ]]
+	then
+		pacstrap /mnt --needed chromium
+	fi
+	
 fi
 
 umount -R /mnt 2> /dev/null
